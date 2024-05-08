9d0a79979fbae1f5865e4ebbf82929132bbaac63
linsaftw <linsaftw@users.noreply.github.com>
2024-05-07 21:48:57 -0300

    prelogin simplification

--- src/main/java/com/arkflame/authmepremium/listeners/PreLoginListener.java
+++ src/main/java/com/arkflame/authmepremium/listeners/PreLoginListener.java
@@ -11,7 +11,6 @@ import net.md_5.bungee.EncryptionUtil;
 import net.md_5.bungee.api.Callback;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.connection.PendingConnection;
 import net.md_5.bungee.api.event.AsyncEvent;
 import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -28,63 +27,121 @@ public class PreLoginListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPreLogin(PreLoginEvent event) {
-        BungeeCord bungee = BungeeCord.getInstance();
-        PendingConnection pendingConnection = event.getConnection();
+        if (event.getConnection() instanceof InitialHandler) {
+            handleInitialHandler(event, (InitialHandler) event.getConnection());
+        }
+    }
 
-        if (pendingConnection instanceof InitialHandler) {
-            InitialHandler initialHandler = (InitialHandler) pendingConnection;
-            initialHandler.setOnlineMode(true);
+    private void handleInitialHandler(PreLoginEvent event, InitialHandler initialHandler) {
+        initialHandler.setOnlineMode(true);
+        ChannelWrapper ch = getChannel(initialHandler);
+        setPremiumPacketHandler(ch, initialHandler);
+        setPreLoginEventCallback(event, initialHandler);
+    }
 
-            ChannelWrapper ch;
-            try {
-                ch = (ChannelWrapper) HandlerReflectionUtil.getFieldValue(initialHandler, "ch");
-                ch.getHandle().pipeline().get(HandlerBoss.class).setHandler(new PremiumPacketHandler(initialHandler));
+    private ChannelWrapper getChannel(InitialHandler initialHandler) {
+        try {
+            return (ChannelWrapper) HandlerReflectionUtil.getFieldValue(initialHandler, "ch");
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-                Callback<PreLoginEvent> callback = new Callback<PreLoginEvent>() {
+    private void setPremiumPacketHandler(ChannelWrapper ch, InitialHandler initialHandler) {
+        if (ch != null) {
+            ch.getHandle().pipeline().get(HandlerBoss.class).setHandler(new PremiumPacketHandler(initialHandler));
+        }
+    }
 
-                    @Override
-                    public void done(PreLoginEvent result, Throwable error) {
-                        if (result.isCancelled()) {
-                            BaseComponent reason = result.getReason();
-                            initialHandler.disconnect((reason != null) ? reason
-                                    : TextComponent.fromLegacy(bungee.getTranslation("kick_message")));
-                            return;
-                        }
-                        if (ch.isClosing()) {
-                            return;
-                        }
-                        if (premium.contains(initialHandler.getName())
-                                || (initialHandler.isOnlineMode() && !notPremium.contains(initialHandler.getName()))) {
-                            try {
-                                EncryptionRequest request = EncryptionUtil.encryptRequest();
-                                Field requestField = InitialHandler.class.getDeclaredField("request");
-                                requestField.setAccessible(true);
-                                requestField.set(initialHandler, request);
-                                initialHandler.unsafe().sendPacket(request);
-                                notPremium.add(initialHandler.getName());
-                            } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException
-                                    | SecurityException e) {
-                                e.printStackTrace();
-                            }
-                        } else {
-                            try {
-                                Method finishMethod = initialHandler.getClass().getDeclaredMethod("finish");
-                                finishMethod.setAccessible(true);
-                                finishMethod.invoke(initialHandler);
-                            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException
-                                    | SecurityException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                };
+    private void setPreLoginEventCallback(PreLoginEvent event, InitialHandler initialHandler) {
+        Callback<PreLoginEvent> callback = createCallback(initialHandler);
+        setCallbackForPreLoginEvent(event, callback);
+    }
 
-                Field doneField = AsyncEvent.class.getDeclaredField("done");
-                doneField.setAccessible(true);
-                doneField.set(event, callback);
-            } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-                e.printStackTrace();
+    private Callback<PreLoginEvent> createCallback(InitialHandler initialHandler) {
+        return new Callback<PreLoginEvent>() {
+            @Override
+            public void done(PreLoginEvent result, Throwable error) {
+                handleCallbackResult(result, error, initialHandler);
             }
+        };
+    }
+
+    private void handleCallbackResult(PreLoginEvent result, Throwable error, InitialHandler initialHandler) {
+        if (result.isCancelled()) {
+            handleCancellation(result, initialHandler);
+        } else if (!isChannelClosing(initialHandler)) {
+            handleChannelNotClosing(result, initialHandler);
+        }
+    }
+
+    private void handleCancellation(PreLoginEvent result, InitialHandler initialHandler) {
+        BaseComponent reason = result.getReason();
+        initialHandler.disconnect((reason != null) ? reason
+                : TextComponent.fromLegacy(BungeeCord.getInstance().getTranslation("kick_message")));
+    }
+
+    private boolean isChannelClosing(InitialHandler initialHandler) {
+        return getChannel(initialHandler).isClosing();
+    }
+
+    private void handleChannelNotClosing(PreLoginEvent result, InitialHandler initialHandler) {
+        if (isPremium(initialHandler) || (initialHandler.isOnlineMode() && !isNotPremium(initialHandler))) {
+            handlePremium(initialHandler);
+        } else {
+            handleNonPremium(initialHandler);
+        }
+    }
+
+    private boolean isPremium(InitialHandler initialHandler) {
+        return PreLoginListener.premium.contains(initialHandler.getName());
+    }
+
+    private boolean isNotPremium(InitialHandler initialHandler) {
+        return PreLoginListener.notPremium.contains(initialHandler.getName());
+    }
+
+    private void handlePremium(InitialHandler initialHandler) {
+        try {
+            sendEncryptionRequest(initialHandler);
+            PreLoginListener.notPremium.add(initialHandler.getName());
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendEncryptionRequest(InitialHandler initialHandler)
+            throws IllegalAccessException, NoSuchFieldException {
+        EncryptionRequest request = EncryptionUtil.encryptRequest();
+        Field requestField = InitialHandler.class.getDeclaredField("request");
+        requestField.setAccessible(true);
+        requestField.set(initialHandler, request);
+        initialHandler.unsafe().sendPacket(request);
+    }
+
+    private void handleNonPremium(InitialHandler initialHandler) {
+        try {
+            invokeFinishMethod(initialHandler);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void invokeFinishMethod(InitialHandler initialHandler)
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method finishMethod = InitialHandler.class.getDeclaredMethod("finish");
+        finishMethod.setAccessible(true);
+        finishMethod.invoke(initialHandler);
+    }
+
+    private void setCallbackForPreLoginEvent(PreLoginEvent preLoginEvent, Callback<PreLoginEvent> callback) {
+        try {
+            Field doneField = AsyncEvent.class.getDeclaredField("done");
+            doneField.setAccessible(true);
+            doneField.set(preLoginEvent, callback);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
         }
     }
 }
